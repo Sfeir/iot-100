d67f09588f557700d09fea713a1a906ab569bb8b
cbalit <dou4872>
2016-11-21 15:38:58 +0100

    initial comit

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+// Generated on 2016-11-21 using
+// generator-webapp 0.5.0
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// If you want to recursively match all subfolders, use:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Configurable paths
+  var config = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    config: config,
+
+    // Watches files for changes and runs tasks based on the changed files
+    watch: {
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= config.app %>/{,*/}*.html',
+          '<%= config.app %>/{,*/}*.js',
+          '<%= config.app %>/images/{,*/}*'
+        ]
+      }
+    },
+
+
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        // Change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      }
+    }
+
+  });
+
+
+  grunt.registerTask('serve', 'start the server and preview your app, --allow-remote for remote access', function (target) {
+
+    grunt.task.run([
+      'connect:livereload',
+      'watch:livereload'
+    ]);
+  });
+
+  grunt.registerTask('default', [
+    'serve'
+  ]);
+};
